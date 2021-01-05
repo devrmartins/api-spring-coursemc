5c539858b6c2b35a866ab0ac7acd1a868e9f0623
Raffael Martins <devrmartins@gmail.com>
2021-01-05 17:47:18 -0300

    Criacao do projeto

---
+++ src/main/java/br/com/devrmartins/cursomc/resources/MainResource.java
@@ -0,0 +1,17 @@
+package br.com.devrmartins.cursomc.resources;
+
+import org.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainResource {
+    @GetMapping
+    public ResponseEntity<String> hello() {
+        JSONObject resp = new JSONObject();
+        resp.put("message","Welcome to the Spring API");
+        return new ResponseEntity<String>(resp.toString(), HttpStatus.OK);
+    }
+}
